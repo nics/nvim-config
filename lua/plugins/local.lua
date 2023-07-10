77d6460be5b290400bcaf5234305281ba889f959
nsteenla <nicolas.steenlant@ugent.be>
2023-07-10 16:04:33 +0200

    manage go imports on save

--- lua/plugins/local.lua
+++ lua/plugins/local.lua
@@ -1,45 +1,8 @@
 return {
   -- colorscheme
   -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   opts = {
-  --     integrations = {
-  --       alpha = true,
-  --       cmp = true,
-  --       gitsigns = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       navic = { enabled = true },
-  --       neotest = true,
-  --       noice = true,
-  --       notify = true,
-  --       neotree = true,
-  --       semantic_tokens = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       which_key = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin-macchiato",
-  --   },
+  --   "folke/tokyonight.nvim",
+  --   opts = { style = "storm" },
   -- },
   -- go
   {
@@ -54,7 +17,7 @@ return {
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
-    -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
   -- go quicktemplate
   {
