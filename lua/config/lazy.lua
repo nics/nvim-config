f659cf38fa0aba72a0de6818ce76b136eed42b64
nsteenla <nicolas.steenlant@ugent.be>
2023-08-29 11:40:02 +0200

    use lazyvim go plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/local.lua
+++ lua/plugins/local.lua
@@ -24,21 +24,6 @@ return {
   --   "folke/tokyonight.nvim",
   --   opts = { style = "storm" },
   -- },
-  -- go
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
   -- go quicktemplate
   {
     "b0o/quicktemplate.vim",
